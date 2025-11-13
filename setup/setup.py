edd34f3847caf1ca3a0f1ba68d75d6c9dcf9d89d
JamesBedsonUPF <84134199+JamesBedsonUPF@users.noreply.github.com>
2025-11-13 15:35:56 +0100

    Moved research to another repository

--- setup/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="usat_designer",
-    version="0.1.0",
-    packages=find_packages(include=["usat_designer", "usat_designer.*"]),
-    install_requires=[],
-    include_package_data=True,
-)
